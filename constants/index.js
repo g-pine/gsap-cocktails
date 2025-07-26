4394d0459ab5ec8395f64e9d8cd09af2bb57a18c
g-pine <pinogallegos.gabriel@gmail.com>
2025-07-26 12:16:08 -0600

    implement the first part of the hero section

---
+++ constants/index.js
@@ -0,0 +1,6 @@
+export const navLinks = [
+  {id: 'cocktails', title: 'Cocktails'},
+  {id: 'about', title: 'About Us'},
+  {id: 'art', title: 'The Art'},
+  {id: 'contact', title: 'Contact'},
+]   
---
+++ src/components/Hero.jsx
@@ -0,0 +1,77 @@
+import gsap from "gsap"
+import { ScrollTrigger, SplitText } from "gsap/all"
+import { useGSAP } from "@gsap/react"
+
+const Hero = () => {
+    useGSAP(() => {
+        const heroSPlit = new SplitText('.title', { type: 'chars, words' });
+        const paragraphSplit = new SplitText('.subtitle', { type: 'lines' });
+    
+        heroSPlit.chars.forEach((char) => char.classList.add('text-gradient'));
+        gsap.from(heroSPlit.chars, {
+            yPercent: 100,
+            duration: 1.8,
+            ease: 'expo.out',
+            stagger: 0.06,
+        });
+
+        gsap.from(paragraphSplit.lines, {
+            opacity: 0,
+            yPercent: 100,
+            duration: 1.8,
+            ease: 'expo.out',
+            stagger: 0.06,
+            delay: 1,
+        });
+        gsap.timeline({ 
+            scrollTrigger: {
+                trigger: '#hero',
+                start: 'top top',
+                end: 'bottom top',
+                scrub: true,
+            }
+        })
+            .to('.right-leaf', { 
+                y: 200 }, 0)
+            .to('.left-leaf', {
+                y: -200 }, 0)
+    }, []);
+
+  return (
+    <>
+        <section id="hero" className="noisy">
+            <h1 className='title'>
+                Mojito
+            </h1>
+            <img
+                src='/images/hero-left-leaf.png'
+                alt='Left Leaf'
+                ckassName='left-leaf'
+            />
+            <img
+                src='/images/hero-right-leaf.png'
+                alt='right-leaf'
+                className='right-leaf'
+            />
+            <div className="body">
+                <div className="content"> 
+                    <div className='space-y-5 hidden md:block'>
+                        <p>Cool. Crisp. Classic</p>
+                        <p className="subtitle">Sip The Spirit <br/> of Summer</p>
+                    </div>
+                    <div className="view-cocktails">
+                        <p className="subtitle">
+                            Every cocktail on our menu is a blend of premium ingredients, creative, flair, and timeless recipes - designed to delight your senses
+                        </p>
+                        <a href="#cocktails">
+                            View Cocktails
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </>
+  )
+}
+
+export default Hero
