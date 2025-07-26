4a5949159bb7ed4617835cc52589b33afe27f27a
g-pine <pinogallegos.gabriel@gmail.com>
2025-07-26 15:42:20 -0600

    implement hero scroll animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,14 @@
 import gsap from "gsap"
 import { ScrollTrigger, SplitText } from "gsap/all"
 import { useGSAP } from "@gsap/react"
+import { useRef } from "react"
+import { useMediaQuery } from "react-responsive"
 
 const Hero = () => {
+
+    const videoRef = useRef();
+    const isMobile = useMediaQuery({ maxWidth: 767 });
+
     useGSAP(() => {
         const heroSPlit = new SplitText('.title', { type: 'chars, words' });
         const paragraphSplit = new SplitText('.subtitle', { type: 'lines' });
@@ -35,6 +41,25 @@ const Hero = () => {
                 y: 200 }, 0)
             .to('.left-leaf', {
                 y: -200 }, 0)
+        
+        const startValue = isMobile ? 'top 50%' : 'center 60%';
+        const endValue = isMobile ? '120% top' : 'bottom top';
+
+        const tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: 'video',
+                start: startValue,
+                end: endValue,
+                scrub: true,
+                pin: true,
+            }
+        });
+
+        videoRef.current.onloadedmetadata = () => {
+            tl.to(videoRef.current, {
+                currentTime: videoRef.current.duration
+            });
+        };
     }, []);
 
   return (
@@ -46,7 +71,7 @@ const Hero = () => {
             <img
                 src='/images/hero-left-leaf.png'
                 alt='Left Leaf'
-                ckassName='left-leaf'
+                className='left-leaf'
             />
             <img
                 src='/images/hero-right-leaf.png'
@@ -70,6 +95,15 @@ const Hero = () => {
                 </div>
             </div>
         </section>
+        <div className="video absolute inset-0">
+            <video
+                ref={videoRef}
+                muted 
+                playsInline
+                preload="auto"
+                src="/videos/output.mp4"
+            />
+        </div>
     </>
   )
 }
